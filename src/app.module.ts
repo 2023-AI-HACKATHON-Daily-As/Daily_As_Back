1b8701bb2d98813a10af97cf3c2eae99cd8d5174
mark_kim <mark340v@gmail.com>
2023-07-25 15:49:43 +0900

    feat: config 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { ChallengesModule } from './challenges/challenges.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
     MongooseModule.forRoot('mongodb://localhost/nest'),
     UsersModule,
     TasksModule,
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+  const configService = app.get(ConfigService);
+  const port = configService.get('server.port');
+  const swaggerConfig = new DocumentBuilder()
     .setTitle(`Daily As Doc's`)
     .setDescription('Daily As API description')
     .setVersion('1.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
-  console.log(`🚀Application is running on: ${await app.getUrl()}`);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  await app.listen(port);
+  Logger.log(`🚀Application is running on: ${port}`);
 }
 bootstrap();
