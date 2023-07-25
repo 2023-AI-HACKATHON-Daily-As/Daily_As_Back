80c7d82ac2d612090cdfb4cdac7eba3e360bca41
mark_kim <mark340v@gmail.com>
2023-07-25 15:06:21 +0900

    feat: class-validator class-transformer swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(`Daily As Doc's`)
+    .setDescription('Daily As API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
   console.log(`🚀Application is running on: ${await app.getUrl()}`);
 }
